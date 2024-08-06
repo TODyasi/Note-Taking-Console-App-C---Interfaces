ab402fcc96b0a57a5dcb14471d981811b72ed537
TODyasi <166889804+TODyasi@users.noreply.github.com>
2024-08-06 10:52:11 +0000

    Add files via upload

---
+++ NoteChive/NoteClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class NoteClass
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+       
+        public void GetUserInput()
+        {
+            string userTitle;
+            string userDescription;
+
+            Console.WriteLine("Add title");
+            userTitle = Console.ReadLine();
+
+            Console.WriteLine("Add description");
+            userDescription = Console.ReadLine();
+
+            this.Title = userTitle;
+            this.Description = userDescription;
+
+        }
+        public override string ToString()
+        {
+            return $"Title: {Title}\nDescription: {Description}";
+        }      
+    }
+}
---
+++ NoteChive/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NotesManager notesManager = new NotesManager();
+            notesManager.AddNotes();
+            notesManager.DisplayNotes();
+
+        }
+    }
+}
