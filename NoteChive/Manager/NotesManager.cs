2d245eb1e630f05c5ac4a313513742a5535ce3b2
TODyasi <166889804+TODyasi@users.noreply.github.com>
2024-08-07 14:53:33 +0000

    Add files via upload

---
+++ NoteChive/Manager/NotesManager.cs
@@ -0,0 +1,58 @@
+﻿using NoteChive.Notes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteChive.Manager
+{
+    public class NotesManager : INoteManager
+    {
+        private List<NoteClass> Notes;
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+
+
+        public NotesManager()
+        {
+            Notes = new List<NoteClass>();
+        }
+
+        public void AddNote()
+        {
+            string userTitle;
+            string userDescription;
+
+            Console.WriteLine("Add title");
+            userTitle = Console.ReadLine();
+
+            Console.WriteLine("Add description");
+            userDescription = Console.ReadLine();
+
+            Title = userTitle;
+            Description = userDescription;
+
+        }
+
+        public void DeleteNote()
+        {
+            Console.WriteLine("Delete note method works");
+        }
+
+        public void DisplayNote()
+        {
+            Console.WriteLine("Display note method works");
+        }
+
+        public void EditNote()
+        {
+            Console.WriteLine("Edit note method works");
+        }
+        public override string ToString()
+        {
+            return $"Title: {Title}\nDescription: {Description}";
+        }
+    }
+}
--- NoteChive/Program.cs
+++ NoteChive/Program.cs
@@ -1,12 +1,15 @@
-﻿namespace ToDoList
+﻿using NoteChive.MainMenu;
+using NoteChive.MainMenu.MainMenu;
+
+namespace ToDoList
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            NotesManager notesManager = new NotesManager();
-            notesManager.AddNotes();
-            notesManager.DisplayNotes();
+            MainMenu menu = new MainMenu();
+            menu.DisplayMenu();
+        
 
         }
     }
