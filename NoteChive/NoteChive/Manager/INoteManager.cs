0eef21aec45c0d2e796b6acbef8613b65f8c40fd
Okuhle dyasi <166889804+TODyasi@users.noreply.github.com>
2024-08-20 08:12:55 +0000

    Add files via upload

---
+++ NoteChive/NoteChive/Manager/INoteManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteChive.Manager
+{
+    public interface INoteManager
+    {
+        void AddNote();
+        void DisplayNote();
+        void DeleteNote();
+        void EditNote();
+    }
+}
---
+++ NoteChive/NoteChive/Program.cs
@@ -0,0 +1,16 @@
+﻿using NoteChive.MainMenu;
+using NoteChive.MainMenu.MainMenu;
+
+namespace ToDoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu menu = new MainMenu();
+            menu.DisplayMenu();
+        
+
+        }
+    }
+}
